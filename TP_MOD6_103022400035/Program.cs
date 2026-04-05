b6ebb7b9c9a49577937380a6ea41154469d9ba45
petraaps <petrasimangunsong07@gmail.com>
2026-04-06 01:26:43 +0700

    menambahkan class SayaMusicTrack

--- TP_MOD6_103022400035/Program.cs
+++ TP_MOD6_103022400035/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    // Constructor
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+        this.playCount = 0;
+
+        // Generate random 5 digit id
+        Random rand = new Random();
+        this.id = rand.Next(10000, 100000);
+    }
+
+    // Method untuk menambah play count
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    // Method untuk print detail track
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("ID       : " + id);
+        Console.WriteLine("Title    : " + title);
+        Console.WriteLine("PlayCount: " + playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat objek
+        SayaMusicTrack track1 = new SayaMusicTrack("Hati-Hati di Jalan");
+
+        // Menambah play count
+        track1.IncreasePlayCount(5);
+        track1.IncreasePlayCount(3);
+
+        // Menampilkan detail
+        track1.PrintTrackDetails();
+    }
+}
